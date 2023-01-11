c5d27eda386bb1c869533ca04ab63fa5a2e5d995
george weinberg <gweinberg@fastmail.fm>
2023-01-11 12:34:47 -0800

    grepgroup.py

---
+++ grepgroup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Display grep capture group 1 if it exists"""
+import sys, re
+
+if __name__ == '__main__':
+    pattern = re.compile(sys.argv[1])
+    for line in sys.stdin:
+         match = re.search(pattern, line)
+         if match:
+             print(match.group(1))
